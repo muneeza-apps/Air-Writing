f5be6bb672039718bb8b20f58f76175e2a0d7607
Your Full Name <your@email.com>
2026-04-29 08:05:58 +0500

    Fix JSON type casting bug for hand tracking

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2584034007" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "269250830" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
